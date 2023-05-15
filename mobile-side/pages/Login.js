dac677d4af09d4f518251f9cbbf714f3df58f0c8
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-05-15 11:54:29 +0700

    feat: lgin system

---
+++ mobile-side/pages/Login.js
@@ -0,0 +1,172 @@
+import { ActivityIndicator, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useContext, useEffect, useMemo, useState } from "react";
+import lang from "../language/language.json";
+import storage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Login() {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const { signIn } = useContext(AuthContext)
+  const [loading, setLoading] = useState(false);
+  const [currLang, setCurrLang] = useState("id");
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigation()
+
+  useEffect(() => {
+    (async () => {
+      let language = await storage.getItem("language");
+      if (!language) await storage.setItem("language", "en");
+      setCurrLang(language || "en");
+    })();
+  }, []);
+
+  const disabled = useMemo(() => {
+    return Object.values(state).some((el) => !el);
+  }, [state]);
+
+  const reset = () => {
+    setState({
+      email: "",
+      password: "",
+    });
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    signIn(state).finally(() => setLoading(false))
+
+  };
+
+  const changeLang = async (val) => {
+    setLoading(true);
+    try {
+      await storage.setItem("language", val);
+      setCurrLang(val);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading)
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0080ff" />
+      </View>
+    );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <View style={{ position: "absolute", zIndex: 10, top: 0, right: 20, flexDirection: "row" }}>
+          <Text onPress={() => changeLang("en")} style={currLang == "en" ? styles.activeToggle : styles.toggle}>
+            EN
+          </Text>
+          <Text onPress={() => changeLang("id")} style={currLang == "id" ? styles.activeToggle : styles.toggle}>
+            ID
+          </Text>
+        </View>
+        <Image
+          source={{ uri: "https://clipground.com/images/png-tracking-1.png" }}
+          style={{
+            width: 120,
+            height: 120,
+          }}
+        />
+        <View style={styles.card}>
+          <Text style={{ fontSize: 25, marginVertical: 15 }}>Login</Text>
+          {errorMsg && (
+            <View style={{ borderWidth: 1, borderColor: "red", padding: 7 }}>
+              <Text style={{ color: "red" }}>{errorMsg}</Text>
+            </View>
+          )}
+
+          <TextInput
+            value={state.email}
+            onChangeText={val => setState({ ...state, email: val })}
+            style={styles.input}
+            placeholder={'Email'}
+          />
+          <TextInput
+            secureTextEntry={true}
+            value={state.password}
+            onChangeText={(val) => setState({ ...state, password: val })}
+            style={styles.input}
+            placeholder={lang[currLang].password}
+          />
+
+          <TouchableOpacity disabled={disabled} onPress={handleSubmit} style={[styles.btn, { opacity: disabled ? 0.5 : 1 }]}>
+            <Text style={styles.textBtn}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_ => navigate.navigate('Register')}>
+            <Text style={{ textAlign: 'right', color: '#0080ff', marginTop: 15, fontSize: 13 }}>Don't have any account?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 15,
+    marginTop: 20,
+    justifyContent: "center",
+    position: "relative",
+  },
+  activeToggle: { width: 35, textAlign: "center", backgroundColor: "#0080ff", color: "white", padding: 5, borderRadius: 3 },
+  toggle: { borderRadius: 3, width: 35, textAlign: "center", padding: 5 },
+  card: {
+    borderRadius: 10,
+    width: "100%",
+    padding: 15,
+    marginTop: 20,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    backgroundColor: "white",
+  },
+  input: {
+    backgroundColor: "#f4f4f4",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginVertical: 5,
+  },
+  btn: {
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 20,
+    backgroundColor: "#0080ff",
+  },
+  btnReport: {
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 20,
+    color: "white",
+    backgroundColor: "red",
+  },
+  textBtn: {
+    textAlign: "center",
+    fontWeight: 600,
+    color: "white",
+  },
+  img: {
+    width: 130,
+    height: 160,
+    marginHorizontal: "30%",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+});
---
+++ mobile-side/pages/home.js
@@ -0,0 +1,226 @@
+import { ActivityIndicator, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { openCamera } from "../helpers";
+import { useContext, useEffect, useMemo, useState } from "react";
+import * as Location from "expo-location";
+import axios from "axios";
+import lang from "../language/language.json";
+import storage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Home() {
+  const [takedImage, setTakedImage] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [currLang, setCurrLang] = useState("id");
+  const [state, setState] = useState({
+    name: "",
+    ages: "",
+    from: "",
+    description: "",
+  });
+
+  const { signOut } = useContext(AuthContext)
+
+
+  const handleCamera = async () => {
+    try {
+      const { assets } = await openCamera();
+      setTakedImage(assets[0].uri);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let language = await storage.getItem("language");
+      if (!language) await storage.setItem("language", "en");
+      setCurrLang(language || "en");
+      console.log('ass')
+    })();
+  }, []);
+
+  const getCurrLoc = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+    return await Location.getCurrentPositionAsync({});
+  };
+
+  const disabled = useMemo(() => {
+    return Object.values(state).some((el) => !el);
+  }, [state, takedImage]);
+
+  const reset = () => {
+    setState({
+      name: "",
+      ages: "",
+      from: "",
+      description: "",
+    });
+    setTakedImage("");
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const location = await getCurrLoc();
+      const formData = new FormData();
+
+      formData.append("name", state.name);
+      formData.append("age", state.ages);
+      formData.append("from", state.from);
+      formData.append("description", state.description);
+      if (takedImage) formData.append("image", { name: new Date().getTime() + "_img", uri: takedImage, type: "image/jpeg" });
+      formData.append("long", location.coords.longitude);
+      formData.append("lat", location.coords.latitude);
+      const res = await axios.post("https://wild-flannel-shirt-foal.cyclic.app/report", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      alert(res.data?.message);
+      reset();
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const changeLang = async (val) => {
+    setLoading(true);
+    try {
+      await storage.setItem("language", val);
+      setCurrLang(val);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading)
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0080ff" />
+      </View>
+    );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <View style={{
+          width: '100%', position: "absolute", justifyContent: "space-between", zIndex: 10,
+          paddingHorizontal: 8,
+          top: 0, right: 20, flexDirection: "row"
+        }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text onPress={() => changeLang("en")} style={currLang == "en" ? styles.activeToggle : styles.toggle}>
+              EN
+            </Text>
+            <Text onPress={() => changeLang("id")} style={currLang == "id" ? styles.activeToggle : styles.toggle}>
+              ID
+            </Text>
+          </View>
+          <TouchableOpacity onPress={() => signOut()}>
+            <Text>Logout</Text>
+          </TouchableOpacity>
+        </View>
+        <Image
+          source={{ uri: "https://clipground.com/images/png-tracking-1.png" }}
+          style={{
+            width: 120,
+            height: 120,
+          }}
+        />
+        <View style={styles.card}>
+          {errorMsg && (
+            <View style={{ borderWidth: 1, borderColor: "red", padding: 7 }}>
+              <Text style={{ color: "red" }}>{errorMsg}</Text>
+            </View>
+          )}
+
+          <TextInput value={state.name} onChangeText={(val) => setState({ ...state, name: val })} style={styles.input} placeholder={lang[currLang].name} />
+
+          <TextInput value={state.ages} onChangeText={(val) => setState({ ...state, ages: val })} keyboardType="number-pad" style={styles.input} placeholder={lang[currLang].ages} />
+
+          <TextInput value={state.from} onChangeText={(val) => setState({ ...state, from: val })} style={styles.input} placeholder={lang[currLang].from} />
+
+          <TextInput multiline={true} numberOfLines={4} style={styles.input} textAlignVertical="top" placeholder={lang[currLang].description} value={state.description} onChangeText={(val) => setState({ ...state, description: val })} />
+
+          {takedImage && <Image source={{ uri: takedImage }} style={{ width: 100, height: 120 }} />}
+
+          <TouchableOpacity onPress={handleCamera} style={styles.btn}>
+            <Text style={styles.textBtn}>{lang[currLang]["upload-image"]}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity disabled={disabled} onPress={handleSubmit} style={[styles.btnReport, { opacity: disabled ? 0.3 : 1 }]}>
+            <Text style={styles.textBtn}>{lang[currLang].submit}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 15,
+    marginTop: 20,
+    justifyContent: "center",
+    position: "relative",
+  },
+  activeToggle: { width: 35, textAlign: "center", backgroundColor: "#0080ff", color: "white", padding: 5, borderRadius: 3 },
+  toggle: { borderRadius: 3, width: 35, textAlign: "center", padding: 5 },
+  card: {
+    borderRadius: 10,
+    width: "100%",
+    padding: 15,
+    marginTop: 20,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    backgroundColor: "white",
+  },
+  input: {
+    backgroundColor: "#f4f4f4",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginVertical: 5,
+  },
+  btn: {
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 20,
+    backgroundColor: "#0080ff",
+  },
+  btnReport: {
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 20,
+    color: "white",
+    backgroundColor: "red",
+  },
+  textBtn: {
+    textAlign: "center",
+    fontWeight: 600,
+    color: "white",
+  },
+  img: {
+    width: 130,
+    height: 160,
+    marginHorizontal: "30%",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+});
